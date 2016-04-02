98c16d9a08e231b70839ed04b7ab3883c688d130
sotdjin <sotdjin@gmail.com>
2016-04-02 01:02:49 +0000

    last minute changes

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe Movie do
   describe 'searching similar directors' do
     it 'should call Movie with director' do
-      Movie.should_receive(:similar_directors).with('Star Wars')
-      Movie.similar_directors('Star Wars')
+      Movie.should_receive(:movies_by_director).with('Star Wars')
+      Movie.movies_by_director('Star Wars')
     end
   end
 end
